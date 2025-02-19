946fd216c5df8a0250860590aedc8ddb6d9e4ca4
agim221 <muhamadagim221@gmail.com>
2025-02-19 08:02:27 +0700

    Add initial application setup and configuration files for api-gateway, order-service, and inventory-service

--- order-service/src/main/java/com/techie/microservice/order/service/OrderService.java
+++ order-service/src/main/java/com/techie/microservice/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.techie.microservice.order.service;
 
+import com.techie.microservice.order.client.InventoryClient;
 import com.techie.microservice.order.dto.OrderRequest;
 import com.techie.microservice.order.model.Order;
 import com.techie.microservice.order.repository.OrderRespository;
@@ -12,14 +13,20 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRespository orderRespository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
+        var isProductInStock =  inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
 
-        orderRespository.save(order);
+        if (isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRespository.save(order);
+       } else {
+           throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
+       }
     }
 }
